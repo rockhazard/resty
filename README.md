e33bbdeee65381a07961c4a7c8dc4be473100ac0
rockhazard <rockhazardz@gmail.ocm>
2020-02-15 04:35:30 -0500

    Add generate csv and report timer.

--- README.md
+++ README.md
@@ -2,25 +2,26 @@
 
 ### Come Again?
 
-This generates a report of [HTTP REST status codes](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes) for given domains.
+This generates a report of [HTTP REST status codes](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes) for the given domains.
 
 ### Why?
 
 So folks can figure out if their domains are working properly.
 
 ### Features
-- Reports HTTP REST status codes for an arbitrary number of given urls.
-- Tests the SSL certificates for given urls, reporting any errors.
+- Retrieves HTTP REST status codes for an arbitrary number of given urls.
+- Outputs report to terminal, csv file, or both.
+- Tests SSL certificates for given urls and reports errors.
 
 ### Dependencies
 If not using the binaries, Python 3.6+ and the requests library are required.
 
 ### Usage
 
-type `resty.py -h` at a terminal.
+Type `resty.py -h` at a terminal.
 
 ### ToDo
 
 - Prune bookmark files of dead links
 - Accept json and csv files as input
-- Output report in json, csv, and plain text file, instead of just to stdout.
+- Output report in json.
--- resty.py
+++ resty.py
@@ -10,16 +10,22 @@ Author: Ike Davis
 
 import sys
 import argparse
+import os
+import csv
+from time import time
+from pathlib import Path
 from textwrap import dedent
 from requests import get, exceptions as reqx
 
 
-class Resty(object):
+class Resty():
     """domain querying class"""
 
     def __init__(self):
         super(Resty, self).__init__()
-        self.domainsList = []
+        self._state = dict(quiet=False, timer=False)
+        self.home = os.path.expanduser('~')
+        self.msgList = []
         self.codes = {
             100: 'Continue',
             101: 'Switching Protocols',
@@ -82,37 +88,63 @@ class Resty(object):
             522: 'Connection timed out',
             524: 'A timeout occurred'}
 
+    def set_state(self, key, value):
+        self._state[key] = value
+
+    def get_state(self, key):
+        return self._state.get(key, None)
+
+    def time_proc(self, sec):
+        mins = sec // 60
+        sec = sec % 60
+        hours = mins // 60
+        mins = mins % 60
+        print("Finished in {0}:{1}:{2}".format(int(hours), int(mins), sec))
+
     def get_urls(self, urls):
         with open(urls) as domains:
             # strip duplicate urls
             domainsSet = set(domains.read().splitlines())
             return list(domainsSet)
 
+    def generate_report(self, messages):
+        csv_doc = str(Path(self.home, 'url_status_codes.csv'))
+        with open(csv_doc.format(self.home), 'w', newline='') as csvfile:
+            status_writer = csv.writer(csvfile, delimiter=',',
+                                       quotechar='\'', quoting=csv.QUOTE_MINIMAL)
+            for message in messages:
+                status_writer.writerow(
+                    [message[0]] + [message[1]] + [message[2]])
+
     def get_status_codes(self, urlList, timeout):
-        msgList = []
         for url in urlList:
             try:
                 code = get(url, timeout=timeout).status_code
                 for status, message in self.codes.items():
                     if status == code:
                         # build list for later processing but print progress
-                        msgList.append([code, url])
-                        print('{}: {} - {}'.format(code, message, url))
+                        self.msgList.append([code, message, url])
+                        if not self._state['quiet']:
+                            print('{}: {} - {}'.format(code, message, url))
             except KeyboardInterrupt:
                 print('Program interrupted by user.')
                 sys.exit(1)
             except reqx.SSLError:
-                msgList.append(['SSL Error', url])
-                print('ERR: SSL Error Detected: {}'.format(url))
+                self.msgList.append(['ERR', 'SSL Error', url])
+                if not self._state['quiet']:
+                    print('ERR: SSL Error Detected: {}'.format(url))
                 continue
             except reqx.MissingSchema:
-                msgList.append(['Invalid URL', url])
-                print('ERR: Invalid URL: {}'.format(url))
+                self.msgList.append(['ERR', 'Invalid URL', url])
+                if not self._state['quiet']:
+                    print('ERR: Invalid URL: {}'.format(url))
                 continue
             except:
-                print('ERR: No response: {}'.format(url))
+                if not self._state['quiet']:
+                    print('ERR: No response: {}'.format(url))
                 continue
-        return msgList
+
+        return self.msgList
 
     def get_1_code(self, url, timeout):
         try:
@@ -153,14 +185,28 @@ def main(*args):
     parser.add_argument('-u', '--urls',
                         help='Report REST status codes for urls in URLS_FILE.',
                         nargs=1, metavar=('URLS_FILE'))
+    parser.add_argument('-r', '--report',
+                        help='Write REST status csv for urls in URLS_FILE.',
+                        nargs=1, metavar=('URLS_FILE'))
     parser.add_argument('-s', '--status',
                         help='Report the REST status code for a single url.',
                         nargs=1, metavar=('URL'))
     parser.add_argument('-t', '--timeout',
                         help='Set HTTP requests timeout in SECONDS (x[.x...]).',
                         nargs=1, metavar=('SECONDS'))
+    parser.add_argument('-q', '--quiet',
+                        help='Suppress output of progress to the terminal.',
+                        action='store_true')
+    parser.add_argument('-c', '--timer',
+                        help='Time the completion speed of the report.',
+                        action='store_true')
 
     args = parser.parse_args()
+    if args.timer:
+        resty.set_state('timer', True)
+        start_timer = time()
+    if args.quiet:
+        resty.set_state('quiet', args.quiet)
     if args.timeout:
         try:
             timeout = float(args.timeout[0])
@@ -174,7 +220,15 @@ def main(*args):
         resty.get_status_codes(urls, timeout)
     elif args.status:
         print(resty.get_1_code(args.status[0], timeout))
+    elif args.report:
+        urls = resty.get_urls(args.report[0])
+        resty.get_status_codes(urls, timeout)
+        resty.generate_report(resty.msgList)
 
+    if resty.get_state('timer'):
+        end_timer = time()
+        elapsed = end_timer - start_timer
+        resty.time_proc(elapsed)
 
 
 if __name__ == "__main__":
